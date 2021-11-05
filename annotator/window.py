c7f5351f9f42861d920b072fd21b6d3a098b9b8e
Michael Schmidt <github@schmidt.tw>
2021-11-05 18:17:37 +0100

    Set default size of window

--- annotator/window.py
+++ annotator/window.py
@@ -11,6 +11,7 @@ class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self)
         self.set_title("Epanet Annotator")
+        self.set_default_size(800, 600)
         self.maximize()
         self.connect("destroy", Gtk.main_quit)
 
